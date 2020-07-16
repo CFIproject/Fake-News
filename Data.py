03b8603d08d89360da6c3cd33b2f00389bcc608f
Nush1881 <68315385+Nush1881@users.noreply.github.com>
2020-07-16 16:59:31 +0530

    Tokenization

--- Data.py
+++ Data.py
@@ -1,13 +1,32 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
+import nltk
+nltk.download()
+from nltk.tokenize import word_tokenize
 
+df = pd.read_csv('fake_or_real_news.csv')
+x = df['text']
+y = df['label']
 
-df = pd.read_csv("fake_or_real_news.csv")
+df = df.drop(columns = ['Unnamed: 0'],axis=1)
+df = df.isnull().sum()
 
-y = df.label 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+train_x, cv_x, y_train, cv_y = train_test_split(x_train, y_train, test_size = 0.2)
 
-df = df.drop("Unnamed: 0", axis=1)      
+print("In train = ",train_x.shape[0])
+print("In test = ",x_test.shape[0])
+print("In cv = ",cv_x.shape[0])
 
-X_train, X_test, Y_train, Y_test = train_test_split(df,y,test_size = 0.2, train_size = 0.8)
-X_train, X_cv, Y_train, Y_cv = train_test_split(X_train,Y_train,test_size = 0.2, train_size = 0.8)
+x = x.str.lower()
 
+def identify_tokens(row):
+    text = row['text']
+    tokens = nltk.word_tokenize(text)
+    # taken only words (not punctuation)
+    token_words = [w for w in tokens if w.isalpha()]#numbers are not taken; for loop, if(no num,punc=true)-so only words are storde in token_words
+    return token_words
+
+df['words'] = df.apply(identify_tokens, axis=1)
+df['words']
