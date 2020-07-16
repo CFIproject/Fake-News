db10bf0c48e151a5909edbd95970ef46443d9a72
Pranjal Jadhav <68317079+Pranjal-Jadhav@users.noreply.github.com>
2020-07-16 10:17:33 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,1 +1,13 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv("fake_or_real_news.csv")
+
+y = df.label 
+
+df = df.drop("Unnamed: 0", axis=1)      
+
+X_train, X_test, Y_train, Y_test = train_test_split(df,y,test_size = 0.2, train_size = 0.8)
+X_train, X_cv, Y_train, Y_cv = train_test_split(X_train,Y_train,test_size = 0.2, train_size = 0.8)
+
