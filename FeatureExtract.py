2cb0ddb4ed1971e9afca4b004124178ad914f5e0
Nush1881 <68315385+Nush1881@users.noreply.github.com>
2020-07-19 17:14:15 +0530

    LogReg and SVM added

--- FeatureExtract.py
+++ FeatureExtract.py
@@ -9,6 +9,9 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn import svm
 
 
 df = pd.read_csv('fake_or_real_news.csv')
